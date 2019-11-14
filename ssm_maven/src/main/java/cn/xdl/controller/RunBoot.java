7ccab9fecaff7ed86e1fcf8e7ac362fdf047592f
huizhicai123 <2573636915@qq.com>
2019-11-14 10:14:48 +0800

    测试

---
+++ ssm_maven/src/main/java/cn/xdl/controller/RunBoot.java
@@ -0,0 +1,13 @@
+package cn.xdl.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class RunBoot {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RunBoot.class, args);
+
+	}
+
+}
